b956760d6b1bb3a8aa7a6244b51a6adee060d9bd
James Weatherbee <37006909+JamesWeatherbee@users.noreply.github.com>
2020-09-11 13:14:19 -0400

    Add files via upload
    Fixed function issues.  Assignment works with extra credit works appropriately.

--- IS211_Assignment3.py
+++ IS211_Assignment3.py
@@ -1,29 +1,49 @@
-# the url is: 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
-# path to file, datetime accessed, browser, status of request, request size in bytes
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import csv
 import urllib.request
 import codecs
 import re
+import argparse
 
-url = 'http://s3.amazonaws.com/cuny-is211-spring2015/weblog.csv'
+parser = argparse.ArgumentParser()
+parser.add_argument('-url', help='Enter a URL')
+args = parser.parse_args()
 
 
 def downloadData(url):
+    '''Open a user provided URL.
+
+    Args:
+        url(str): The url for a website that should contain a .csv file.
+
+    Returns:
+        csvfile(file): The .csv file from the provided URL.
+    '''
     ftpstream = urllib.request.urlopen(url)
     csvfile = csv.reader(codecs.iterdecode(ftpstream, 'utf-8'))
     return csvfile
 
 
 def processUrl(csvfile):
+    '''Processes the user provided .csv file from the URL.
 
+    Args:
+        csvfile(file): The .csv file from the URL opened with downloadData function.
+
+    Returns:
+        output(str): A string that the number of hits, the number of images and percentage
+        of the hits that are images, and the browser that used the most.
+        hourOutput(list): A list that lists the number of hits per hour.
+    '''
     browserCount = 0
     imageCount = 0
 
-    zeroHour = oneHour = twoHour = threeHour = fourHour = fiveHour = sixHour = 0
+    oneHour = twoHour = threeHour = fourHour = fiveHour = sixHour = 0
     sevenHour = eightHour = nineHour = tenHour = elevenHour = twelveHour = 0
     thirteenHour = fourteenHour = fifteenHour = sixteenHour = seventeenHour = eighteenHour = 0
     nineteenHour = twentyHour = twentyoneHour = twentytwoHour = twentythreeHour = twentyfourHour = 0
-
 
     firefox = ['Firefox', 0]
     safari = ['Safari', 0]
@@ -49,88 +69,96 @@ def processUrl(csvfile):
         output = 'Of {} hits, {} ({}%) are images. The most popular browser is {}.'.format(browserCount, imageCount,
                                                                                            imagePercent,
                                                                                            mostPopularBrowser[0])
-        print(output)
-
         if re.search(r'[0][0]:[0-9][0-9]:[0-9][0-9]', line[1]):
             oneHour += 1
-        if re.search(r'[0][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twoHour += 1
-        if re.search(r'[0][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twoHour += 1
-        if re.search(r'[0][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
             threeHour += 1
-        if re.search(r'[0][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
             fourHour += 1
-        if re.search(r'[0][5]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][5]:[0-9][0-9]:[0-9][0-9]', line[1]):
             fiveHour += 1
-        if re.search(r'[0][6]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][6]:[0-9][0-9]:[0-9][0-9]', line[1]):
             sixHour += 1
-        if re.search(r'[0][7]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][7]:[0-9][0-9]:[0-9][0-9]', line[1]):
             sevenHour += 1
-        if re.search(r'[0][8]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][8]:[0-9][0-9]:[0-9][0-9]', line[1]):
             eightHour += 1
-        if re.search(r'[0][9]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[0][9]:[0-9][0-9]:[0-9][0-9]', line[1]):
             nineHour += 1
-        if re.search(r'[1][0]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][0]:[0-9][0-9]:[0-9][0-9]', line[1]):
             tenHour += 1
-        if re.search(r'[1][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
             elevenHour += 1
-        if re.search(r'[1][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twelveHour += 1
-        if re.search(r'[1][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
             thirteenHour += 1
-        if re.search(r'[1][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
             fourteenHour += 1
-        if re.search(r'[1][5]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][5]:[0-9][0-9]:[0-9][0-9]', line[1]):
             fifteenHour += 1
-        if re.search(r'[1][6]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][6]:[0-9][0-9]:[0-9][0-9]', line[1]):
             sixteenHour += 1
-        if re.search(r'[1][7]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][7]:[0-9][0-9]:[0-9][0-9]', line[1]):
             seventeenHour += 1
-        if re.search(r'[1][8]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][8]:[0-9][0-9]:[0-9][0-9]', line[1]):
             eighteenHour += 1
-        if re.search(r'[1][9]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[1][9]:[0-9][0-9]:[0-9][0-9]', line[1]):
             nineteenHour += 1
-        if re.search(r'[2][0]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[2][0]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twentyHour += 1
-        if re.search(r'[2][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[2][1]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twentyoneHour += 1
-        if re.search(r'[2][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[2][2]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twentytwoHour += 1
-        if re.search(r'[2][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[2][3]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twentythreeHour += 1
-        if re.search(r'[2][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
+        elif re.search(r'[2][4]:[0-9][0-9]:[0-9][0-9]', line[1]):
             twentyfourHour += 1
 
-        print('Hour 1 had {} hits.'.format(oneHour))
-        print('Hour 2 had {} hits.'.format(twoHour))
-        print('Hour 3 had {} hits.'.format(threeHour))
-        print('Hour 4 had {} hits.'.format(fourHour))
-        print('Hour 5 had {} hits.'.format(fiveHour))
-        print('Hour 6 had {} hits.'.format(sixHour))
-        print('Hour 7 had {} hits.'.format(sevenHour))
-        print('Hour 8 had {} hits.'.format(eightHour))
-        print('Hour 9 had {} hits.'.format(nineHour))
-        print('Hour 10 had {} hits.'.format(tenHour))
-        print('Hour 11 had {} hits.'.format(elevenHour))
-        print('Hour 12 had {} hits.'.format(twelveHour))
-        print('Hour 13 had {} hits.'.format(thirteenHour))
-        print('Hour 14 had {} hits.'.format(fourteenHour))
-        print('Hour 15 had {} hits.'.format(fifteenHour))
-        print('Hour 16 had {} hits.'.format(sixteenHour))
-        print('Hour 17 had {} hits.'.format(seventeenHour))
-        print('Hour 18 had {} hits.'.format(eighteenHour))
-        print('Hour 19 had {} hits.'.format(nineteenHour))
-        print('Hour 20 had {} hits.'.format(twentyHour))
-        print('Hour 21 had {} hits.'.format(twentyoneHour))
-        print('Hour 22 had {} hits.'.format(twentytwoHour))
-        print('Hour 23 had {} hits.'.format(twentythreeHour))
-        print('Hour 24 had {} hits.'.format(twentyfourHour))
+    hourOutput = ['Hour 1 had {} hits.'.format(oneHour),
+                  'Hour 2 had {} hits.'.format(twoHour),
+                  'Hour 3 had {} hits.'.format(threeHour),
+                  'Hour 5 had {} hits.'.format(fiveHour),
+                  'Hour 6 had {} hits.'.format(sixHour),
+                  'Hour 7 had {} hits.'.format(sevenHour),
+                  'Hour 8 had {} hits.'.format(eightHour),
+                  'Hour 9 had {} hits.'.format(nineHour),
+                  'Hour 10 had {} hits.'.format(tenHour),
+                  'Hour 11 had {} hits.'.format(elevenHour),
+                  'Hour 12 had {} hits.'.format(twelveHour),
+                  'Hour 13 had {} hits.'.format(thirteenHour),
+                  'Hour 14 had {} hits.'.format(fourteenHour),
+                  'Hour 15 had {} hits.'.format(fifteenHour),
+                  'Hour 16 had {} hits.'.format(sixteenHour),
+                  'Hour 17 had {} hits.'.format(seventeenHour),
+                  'Hour 18 had {} hits.'.format(eighteenHour),
+                  'Hour 19 had {} hits.'.format(nineteenHour),
+                  'Hour 20 had {} hits.'.format(twentyHour),
+                  'Hour 21 had {} hits.'.format(twentyoneHour),
+                  'Hour 22 had {} hits.'.format(twentytwoHour),
+                  'Hour 23 had {} hits.'.format(twentythreeHour),
+                  'Hour 24 had {} hits.'.format(twentyfourHour)]
+
+    print(output)
+    for i in hourOutput:
+        print(i)
 
 
 def main():
-    downloadData(url)
-    processUrl(csvfile)
+    '''Runs the functions downloadData and processUrl if url is valid.  Returns error message if not.'''
+    if not args.url:
+        exit()
+    try:
+        csv_data = downloadData(args.url)
+    except urllib.error.URLError:
+        print('Not a valid URL.')
+    else:
+        processUrl(csv_data)
 
 
 if __name__ == '__main__':
